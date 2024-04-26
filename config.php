6ca430ba5910e765a0c6e13ef09611f10b3bbc3f
mcp7031 <57163587+mcp7031@users.noreply.github.com>
2024-04-26 16:59:46 +1200

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php
-$cid = '1517973100';
-$fourAmigos = '-1001878717768';
-$theRoadToNineveh = '-1001749985334';
-$apiToken = '5874335368:AAHaI8guC4a7lF1rGtzX_oYrgpdyXhUU6-I';
+$cid = '';
+$fourAmigos = '';
+$theRoadToNineveh = '';
+$apiToken = ;
 $bot_api_key  = $apiToken;
-$bot_username = 'freedis_bot';
-$hook_url     = 'https://mini.me/Users/dca/PHPWorkspace/14_RTV/hook.php';
+$bot_username = '';
+$hook_url     = '';
 return [
     'database' => [
         'host'     => 'localhost',
         'port'     => 3306, // optional
-        'username' => 'dca',
-        'password' => 'Katie@1992',
-        'dbname'   => 'rtv',
+        'username' => '',
+        'password' => '',
+        'dbname'   => '',
     ],
     'bot_api_key' => $bot_api_key,
     'bot_username' => $bot_username,
